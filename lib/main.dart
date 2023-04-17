c7c63787235344eb101533864c785d7630ea534e
HunterAdv <gustavopaulody@gmail.com>
2023-04-17 20:38:49 -0300

    20230417: Criando conexão com banco e listagem

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:task_list/task_create.dart';
 import 'package:task_list/task_list.dart';
 
-void main() {
+const firebaseConfig = FirebaseOptions(
+    apiKey: "AIzaSyCZyOrEA4Zh44QVjAfqtxARqtRvbfysQ8c",
+    authDomain: "task-list-dd2e5.firebaseapp.com",
+    projectId: "task-list-dd2e5",
+    storageBucket: "task-list-dd2e5.appspot.com",
+    messagingSenderId: "756975048599",
+    appId: "1:756975048599:web:5b12784750fd8737f8d6d2");
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: firebaseConfig);
   runApp(const MyApp());
 }
 
--- lib/task_list.dart
+++ lib/task_list.dart
@@ -1,12 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'task.dart';
 
 class TaskListPage extends StatelessWidget {
-  List<Task> tasks = [
-    Task('1', 'Task 1'),
-    Task('2', 'Task 2', priority: 'low'),
-    Task('3', 'Task 3', finished: true),
-  ];
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  TaskListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -14,15 +12,25 @@ class TaskListPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Tasks"),
       ),
-      body: ListView(
-          children: tasks
-              .map((task) => CheckboxListTile(
-                    onChanged: (value) {},
-                    value: task.finished,
-                    title: Text(task.name!),
-                    subtitle: Text(task.priority!),
-                  ))
-              .toList()),
+      body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+          stream: firestore.collection('tasks').snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            var tasks = snapshot.data!.docs;
+
+            return ListView(
+                children: tasks
+                    .map((task) => CheckboxListTile(
+                        onChanged: (value) {},
+                        value: task['finished'],
+                        title: Text(task['name'])))
+                    .toList());
+          }),
       floatingActionButton: FloatingActionButton(
         onPressed: () => Navigator.of(context).pushNamed('/task_create'),
         child: const Icon(Icons.add),
